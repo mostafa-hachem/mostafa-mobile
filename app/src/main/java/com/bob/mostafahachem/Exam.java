9b72eae78bb29c4d4579a8f78ac9f8ad3858ed48
mostafa-hachem <mostafa123hachem@gmail.com>
2025-02-16 13:08:12 +0200

    test

--- app/src/main/java/com/bob/mostafahachem/Exam.java
+++ app/src/main/java/com/bob/mostafahachem/Exam.java
@@ -211,7 +211,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+//
 public class Exam extends AppCompatActivity {
 
     private Question[] data;
